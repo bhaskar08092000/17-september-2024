9a85aaad074020fcbf218c55d98bf24ada879b48
bhaskar08092000 <bhaskarkewalramani08@gmail.com>
2024-09-17 16:45:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using CRUD_DBF.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<IshitadfaContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
